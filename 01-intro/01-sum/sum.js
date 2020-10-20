0e2a3652133ac489aa943f7d41bc4e37c1e8428a
Mikhail Ponomarev <ponomarev45@gmail.com>
2020-10-20 18:02:19 +0300

    add solution for 01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw TypeError("Not number");
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
